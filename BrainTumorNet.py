a536a2aefdb27d91961dcef6954dd844cdbad10b
Nam Pho <npho@uw.edu>
2025-05-25 23:43:22 -0700

    place holder for custom model

---
+++ BrainTumorNet.py
@@ -0,0 +1,41 @@
+"""
+BrainTumorNet.py (May 2025)
+"""
+
+import torch
+import torch.nn as nn
+
+class BrainTumorNet(torch.nn.Module):
+	"""
+	A simple neural network architecture for brain tumor classification.
+	"""
+
+	def __init__(self):
+		super(BrainTumorNet, self).__init__()
+		
+		self.cnn1 = nn.Sequential(
+			nn.Conv2d(in_channels=1, out_channels=8, kernel_size=5, stride=1, padding=1),
+			nn.ReLU(),
+			nn.AvgPool2d(kernel_size=2)
+		)
+		
+		self.cnn2 = nn.Sequential(
+			nn.Conv2d(in_channels=8, out_channels=128, kernel_size=3, stride=1, padding=1),
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=2)
+		)
+		
+		l = 500
+		self.fc1 = nn.Sequential(
+			nn.Linear(128*6*6, l),
+			nn.ReLU(),
+			nn.Linear(l, 4)
+		)
+
+	def forward(self, x):
+		out = self.cnn1(x)
+		out = self.cnn2(out)
+		out = out.view(out.size(0), -1)		
+		out = self.fc1(out)
+		
+		return out
